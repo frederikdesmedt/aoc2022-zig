029c14549b9248f0113b9541108da842652396cc
Frederik De Smedt <frederik@dataroots.io>
2022-12-03 15:04:34 +0100

    Implement part 2 of challenge 2

--- challenge2/main.zig
+++ challenge2/main.zig
@@ -2,24 +2,46 @@ const std = @import("std");
 const ArrayList = std.ArrayList;
 const allocator = std.heap.page_allocator;
 
-test "expect program to generate example solution" {
+test "expect program to solve example solution for part 1" {
     const input = [_][]const u8{
         "A Y",
         "B X",
         "C Z"
     };
     const expected: u32 = 15;
-    const actual = try solve(input[0..]);
+    const actual = try solvePart1(input[0..]);
     try std.testing.expectEqual(expected, actual);
 }
 
-fn solve(input: []const[]const u8) GameError!u32 {
+test "expect program to solve example solution for part 2" {
+    const input = [_][]const u8{
+        "A Y",
+        "B X",
+        "C Z"
+    };
+    const expected: u32 = 12;
+    const actual = try solvePart2(input[0..]);
+    try std.testing.expectEqual(expected, actual);
+}
+
+fn solvePart1(input: []const[]const u8) GameError!u32 {
     var totalScore: u32 = 0;
     for (input) |game| {
         const opponentsMove = try Move.parseChar(game[0]);
         const myMove = try Move.parseChar(game[2]);
         const outcome = calculateOutcome(opponentsMove, myMove);
         totalScore += calculateScore(outcome, myMove);
+    }
+    return totalScore;
+}
+
+fn solvePart2(input: []const[]const u8) GameError!u32 {
+    var totalScore: u32 = 0;
+    for (input) |game| {
+        const opponentsMove = try Move.parseChar(game[0]);
+        const desiredOutcome = try Outcome.parseChar(game[2]);
+        const moveToPlay = findMove(opponentsMove, desiredOutcome);
+        totalScore += calculateScore(desiredOutcome, moveToPlay);
     }
     return totalScore;
 }
@@ -36,6 +58,26 @@ fn calculateScore(outcome: Outcome, move: Move) u32 {
         Move.scissors => 3
     };
     return outcomeScore + moveScore;
+}
+
+fn findMove(opponentMove: Move, desiredOutcome: Outcome) Move {
+    return switch (opponentMove) {
+        Move.rock => switch (desiredOutcome) {
+            Outcome.draw => Move.rock,
+            Outcome.won => Move.paper,
+            Outcome.lost => Move.scissors
+        },
+        Move.paper => switch (desiredOutcome) {
+            Outcome.lost => Move.rock,
+            Outcome.draw => Move.paper,
+            Outcome.won => Move.scissors
+        },
+        Move.scissors => switch (desiredOutcome) {
+            Outcome.won => Move.rock,
+            Outcome.lost => Move.paper,
+            Outcome.draw => Move.scissors
+        }
+    };
 }
 
 fn calculateOutcome(opponentMove: Move, myMove: Move) Outcome {
@@ -59,7 +101,8 @@ fn calculateOutcome(opponentMove: Move, myMove: Move) Outcome {
 }
 
 const GameError = error{
-    InvalidMove
+    InvalidMove,
+    InvalidOutcome
 };
 
 const Move = enum{
@@ -83,14 +126,25 @@ const Move = enum{
 const Outcome = enum{
     won,
     lost,
-    draw
+    draw,
+
+    fn parseChar(char: u8) GameError!Outcome {
+        return switch (char) {
+            'X' => Outcome.lost,
+            'Y' => Outcome.draw,
+            'Z' => Outcome.won,
+            else => GameError.InvalidOutcome
+        };
+    }
 };
 
 pub fn main() !void {
     var linesList = try readLines();
     const lines = linesList.toOwnedSlice();
-    const solution = try solve(lines);
-    try std.io.getStdOut().writer().print("Solution: {}\n", .{solution});
+    const solutionPart1 = try solvePart1(lines);
+    try std.io.getStdOut().writer().print("Solution part 1: {}\n", .{solutionPart1});
+    const solutionPart2 = try solvePart2(lines);
+    try std.io.getStdOut().writer().print("Solution part 2: {}\n", .{solutionPart2});
 }
 
 fn readLines() !ArrayList([]const u8) {
